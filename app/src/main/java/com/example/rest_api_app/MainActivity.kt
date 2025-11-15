32d0c0452cf3bb79dd733f38fdaf84b6a7bad8fc
Rayhan <mdrayhan260225@gmail.com>
2025-11-15 23:19:17 +0600

    Intent activity

--- app/src/main/java/com/example/rest_api_app/MainActivity.kt
+++ app/src/main/java/com/example/rest_api_app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.rest_api_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@MainActivity, product_activity::class.java))
+            finish()
+        },4000)
+
     }
 }
