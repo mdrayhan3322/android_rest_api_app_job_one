052fdba0833ca9bc1e31262529a16fcd8377b540
Rayhan <mdrayhan260225@gmail.com>
2025-11-16 18:41:55 +0600

    Retrofit code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit code / link
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
 }
